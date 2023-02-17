27f246d80d277f5622799dd239995df0cda1efca
Endless Ideas <123470607+endlessideastech@users.noreply.github.com>
2023-02-17 22:18:17 +0900

    first commit
    Create initial shell script

---
+++ script.sh
@@ -0,0 +1,33 @@
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# brew upgrade
+# brew install --cask postman
+# brew install --cask dotnet-sdk
+# brew install --cask visual-studio
+
+# brew install --cask visual-studio-code
+# code --install-extension esbenp.prettier-vscode
+code --install-extension rbbit.typescript-hero
+code --install-extension ms-vscode.vscode-node-azure-pack
+code --install-extension ms-azure-devops.azure-pipelines
+code --install-extension VisualStudioOnlineApplicationInsights.application-insights
+code --install-extension ms-azuretools.vscode-bicep
+code --install-extension eamodio.gitlens
+
+
+# brew install --cask microsoft-azure-storage-explorer
+# brew install --cask azure-data-studio
+# brew install azure-cli
+# brew install --cask powershell
+# brew install node
+# brew install angular-cli
+# brew install --cask drawio
+# brew install --cask google-chrome
+# brew install --cask microsoft-remote-desktop
+# brew install --cask microsoft-office
+# brew install --cask skype
+# brew install --cask slack
+# brew install --cask whatsapp
+# brew install --cask zoom
+
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
+brew install --cask iterm2
